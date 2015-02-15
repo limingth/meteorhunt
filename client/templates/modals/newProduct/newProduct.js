fb763f9d00d5cffea8084590f44d270b2bab1d28
farcryzry <farcryzry@gmail.com>
2015-02-15 01:04:26 -0800

    modify chat feature

--- client/templates/modals/newProduct/newProduct.js
+++ client/templates/modals/newProduct/newProduct.js
@@ -1,29 +1,36 @@
 AutoForm.hooks({
-  'products-new-form': {
-    onSuccess: function (operation, result, template) {
-      IonModal.close();
-      Router.go('products.show', {_id: result});
+    'products-new-form': {
+        onSuccess: function (operation, result, template) {
+            IonModal.close();
+            Router.go('products.show', {_id: result});
+        }
     }
-  }
 });
 
 
-
-
 Template.newChat.helpers({
-  name: function () {
-    // http://momentjs.com/
-    if (Meteor.user())
-      return Meteor.user().profile.name;
-    else
-      return "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y"
-  },
+    id: function() {
+        if(Meteor.user()) {
+            return Meteor.user()._id;
+        } else {
+            return 'guest';
+        }
+    },
 
-  avatar_url: function () {
-    // http://momentjs.com/
-    if (Meteor.user())
-      return Meteor.user().profile.avatar_url;
-    else
-      return "Guest";
-  },
+    name: function () {
+        // http://momentjs.com/
+        if (Meteor.user())
+            return Meteor.user().profile.name;
+        else
+            return "Guest";
+    },
+
+    avatar_url: function () {
+        // http://momentjs.com/
+        if (Meteor.user())
+            return Meteor.user().profile.avatar_url;
+        else
+
+            return "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y"
+    }
 });
